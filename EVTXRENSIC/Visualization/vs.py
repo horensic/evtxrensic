7a754647eda1e0f9712a3d1392f25e0ed0ef964d
Kim Dong Hyun <digitalisx99@gmail.com>
2017-12-02 02:57:10 +0900

    Flask Setting

---
+++ EVTXRENSIC/Visualization/vs.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/')
+def hello():
+	return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
